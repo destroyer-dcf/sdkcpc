82f1fae55fda6557cd3c826f5b00f47f278cc381
Destroyer <destroyer.dcf@gmail.com>
2022-06-13 10:44:28 +0200

    modificamos cambio de configuraciones del progrmaa

--- sdkcpc/config.py
+++ sdkcpc/config.py
@@ -1,10 +1,10 @@
 import yaml
 from yaml.loader import SafeLoader
-import pkgutil
 import os.path as path
 from rich.console import Console
 from rich import print
 import os
+from .common import *
 
 from rich.console import Console
 console = Console(width=80,color_system="windows",force_terminal=True)
@@ -49,7 +49,7 @@ def checkConfigKey(key):
     with open(path.dirname(path.abspath(__file__)) + '/sdkcpc.yml', 'r', encoding='utf8') as f:
         try:
             data = yaml.load(f, Loader=SafeLoader)
-            print("[yellow]Chage value Key: [white]" + data[key])
+            print("[yellow]Change value Key ("+key+"): [white]" + data[key])
         except KeyError as e:
             print('[red bold]The key %s does not exist' % str(e))
             exit(1)
@@ -60,12 +60,12 @@ def checkConfigKey(key):
 #   @Param: Nombre de la Clave
 #   @Param: Nuevo valor de la Clave
 def setConfigKeyValue(key,value):
-    checkConfigKey(key)
+    
     if key == "rvm.path" or key == "winape.path":
         if not os.path.exists(value):
             print("[yellow](config sdkcpc)[red bold]\["+key+"]:El archivo " + value + " does not exist.")
             exit(1)
-
+    checkConfigKey(key)
     with open(path.dirname(path.abspath(__file__)) + '/sdkcpc.yml', 'r', encoding='utf8') as f:
         doc = yaml.load(f, Loader=yaml.FullLoader)
 
@@ -73,7 +73,10 @@ def setConfigKeyValue(key,value):
 
     with open(path.dirname(path.abspath(__file__)) + '/sdkcpc.yml', 'w', encoding='utf8') as f:
         yaml.dump(doc, f)
-    getConfigKey(key)
+    print("[yellow]New Value Key    ("+key+"): [green]" + value)
+    print()
+    console.rule("sdkcpc config")
+    listConfigsKeys()
 
 # Lee los datos de configuracion para utilizarlos en el programa
 def loadConfigData():
