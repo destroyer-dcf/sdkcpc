1bdc39a29dff781643f6d74d394de4e33d4d58af
Destroyer <destroyer.dcf@gmail.com>
2022-06-15 12:46:39 +0200

    Cambiamos el texto de about

--- sdkcpc/about.py
+++ sdkcpc/about.py
@@ -18,10 +18,14 @@ console = Console(width=100,color_system="windows",force_terminal=True)
 def about():
     head("6128")
     ver =  __version__
-    banner ="""\n[bold white] ╔═╗╔═╗╔═╗╔═╗╦ ╦ [bold white]┌─────────────┐[/]    [bold white]Created by: [bold green] © Destroyer - 2022[/]
-[bold white] ║  ╠═╝║  ╠═╝╚╦╝ [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Github    : [bold green] https://github.com/destroyer-dcf/sdkcpc[/]
-[bold white] ╚═╝╩  ╚═╝╩   ╩  [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Version   : [bold green] {ver}[/]
-[yellow] SDK Amstrad CPC[bold white] └─────────────┘[/]    [bold white]Follow me : [bold green] twiter: @sdkcpc[/]""".format(ver=ver)
+#     banner ="""\n[bold white] ╔═╗╔═╗╔═╗╔═╗╦ ╦ [bold white]┌─────────────┐[/]    [bold white]Created by: [bold green] © Destroyer - 2022[/]
+# [bold white] ║  ╠═╝║  ╠═╝╚╦╝ [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Github    : [bold green] https://github.com/destroyer-dcf/sdkcpc[/]
+# [bold white] ╚═╝╩  ╚═╝╩   ╩  [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Version   : [bold green] {ver}[/]
+# [yellow] SDK Amstrad CPC[bold white] └─────────────┘[/]    [bold white]Follow me : [bold green] twiter: @sdkcpc[/]""".format(ver=ver)
+    banner ="""\n[bold white] ╔═╗╔╦╗╦╔═╔═╗╔═╗╔═╗ [bold white]┌─────────────┐[/]    [bold white]Created by: [bold green] © Destroyer - 2022[/]
+[bold white] ╚═╗ ║║╠╩╗║  ╠═╝║   [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Github    : [bold green] https://github.com/destroyer-dcf/sdkcpc[/]
+[bold white] ╚═╝═╩╝╩ ╩╚═╝╩  ╚═╝ [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Version   : [bold green] {ver}[/]
+[yellow] For Amstrad Basic[bold white]  └─────────────┘[/]    [bold white]Follow me : [bold green] twitter: @sdkcpc[/]""".format(ver=ver)
 
 
     #print(Panel(banner,expand=True))
