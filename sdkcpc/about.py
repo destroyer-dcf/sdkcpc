4739666fada5ba037153ffa3781c6f8d26b1a101
Destroyer <destroyer.dcf@gmail.com>
2022-06-15 12:51:03 +0200

    cambios de color en about

--- sdkcpc/about.py
+++ sdkcpc/about.py
@@ -25,7 +25,7 @@ def about():
     banner ="""\n[bold white] ╔═╗╔╦╗╦╔═╔═╗╔═╗╔═╗ [bold white]┌─────────────┐[/]    [bold white]Created by: [bold green] © Destroyer - 2022[/]
 [bold white] ╚═╗ ║║╠╩╗║  ╠═╝║   [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Github    : [bold green] https://github.com/destroyer-dcf/sdkcpc[/]
 [bold white] ╚═╝═╩╝╩ ╩╚═╝╩  ╚═╝ [bold white]│ [red bold]■■■[green] ■■■[blue] ■■■ [white]│[/]    [bold white]Version   : [bold green] {ver}[/]
-[yellow] For Amstrad Basic[bold white]  └─────────────┘[/]    [bold white]Follow me : [bold green] twitter: @sdkcpc[/]""".format(ver=ver)
+[white] For Amstrad Basic[bold white]  └─────────────┘[/]    [bold white]Follow me : [bold green] twitter: @sdkcpc[/]""".format(ver=ver)
 
 
     #print(Panel(banner,expand=True))
