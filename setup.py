d6f34b55210f869aab634d8766befb9d2a112547
destroyer <destroyer.dcf@gmail.com>
2022-08-10 20:24:21 +0200

    changes in version and setup.py license.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
         ], 
         python_requires='>=3.6',
         classifiers=[
-            'License :: OSI Approved :: GNU General Public License',
+            'License :: OSI Approved :: GNU General Public License (GPL)',   
             'Programming Language :: Python',
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.6',
