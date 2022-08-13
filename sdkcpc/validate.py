fbb8580f05cbcc349438c42b28818df1963fc8f4
destroyer <destroyer.dcf@gmail.com>
2022-08-13 12:37:07 +0200

    add changes in validate

--- sdkcpc/validate.py
+++ sdkcpc/validate.py
@@ -1,7 +1,21 @@
 #!/usr/bin/python
 import yaml
+import re
+import os, sys
 from cerberus import Validator
 
+PWD                = os.getcwd() + "/"
+MAKEFILE           = "Project.yaml"
+# CONFIG             = loadConfigData()
+FOLDER_PROJECT_NEW = ["OUT","ASCII","BIN","BASIC","OBJ"] 
+FOLDER_PROJECT_8BP = ["ASM","DSK","MUSIC","OUTPUT_SPEDIT","OUT","ASCII","BIN","BASIC","OBJ"] 
+
+
+def show_error_validate(error):
+    new_error = re.sub('[^a-zA-Z0-9:, \n\.]', '', error)
+    new_error = new_error.replace(":", " >")
+    new_error = new_error.replace(",", "\n")
+    print ("ERROR List:\n " + new_error)
 
 def load_doc_project():
     with open('./project.yaml', 'r') as stream:
@@ -10,14 +24,11 @@ def load_doc_project():
         except yaml.YAMLError as exception:
             raise exception
 
-schema = eval(open('./schema.py', 'r').read())
-v = Validator(schema)
-doc = load_doc_project() 
-if not v.validate(doc, schema):
-    x = str(v.errors).replace("{", "")
-    x = x.replace("}", "")
-    x = x.replace("[", "")
-    x = x.replace("]", "")
-    x = x.replace("'", "")
-    x = x.replace(":", " >")
-    print("ERROR: "+ x)
+def validate_data():
+    schema = eval(open('./schema.py', 'r').read())
+    v = Validator(schema)
+    doc = load_doc_project() 
+
+    if not v.validate(doc, schema):
+        show_error_validate(str(v.errors))
+        sys.exit(1)
