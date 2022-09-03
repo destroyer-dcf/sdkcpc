223d7990c9ae64e01da98ad177303f178a959649
destroyer <destroyer.dcf@gmail.com>
2022-09-03 10:42:01 +0200

    refactoring validations

---
+++ sdkcpc/files.py
@@ -0,0 +1,123 @@
+import glob
+from rich.console import Console
+
+
+from .common import *
+from .project import *
+from .validate import *
+import configparser
+import os
+
+from rich import print
+from rich.table import Table
+from rich import box
+from rich.table import Column
+console = Console(width=100,color_system="windows",force_terminal=True)
+contador_files = 0
+
+project_data = Get_data_project_dict()
+
+def info():
+    Section_general = readProyectSection("general")
+    Section_rvm     = readProyectSection("rvm")
+    show_head("Information project","white")
+    # print("[*] ------------------------------------------------------------------------")
+    print("[*] [blue bold]COMPILATION [/]------------------------------------------------------------")
+    print("[*] - compilation: " + project_data["compilation"]["build"])
+    print("[*] - version: " + project_data["compilation"]["version"])
+    print("[*] [blue bold]GENERAL [/]----------------------------------------------------------------")
+    print("[*] - name: " + project_data["general"]["name"])
+    print("[*] - description: " + project_data["general"]["description"])
+    print("[*] - template: " + project_data["general"]["template"])
+    print("[*] - authors: " + project_data["general"]["authors"])
+    print("[*] [blue bold]CONFIG [/]-----------------------------------------------------------------")
+    print("[*] - concatenate.bas.files: " + project_data["config"]["concatenate.bas.files"])
+    print("[*] - name.bas.file: " + project_data["config"]["name.bas.file"])
+    print("[*] [blue bold]RETRO VIRTUAL MACHINE [/]--------------------------------------------------")
+    print("[*] - model.cpc: " + project_data["rvm"]["model.cpc"])
+    print("[*] [blue bold]M4 BOARD [/]---------------------------------------------------------------")
+    print("[*] - ip: " + project_data["m4"]["ip"])
+    print(" ")
+
+    config = configparser.ConfigParser()
+    config.read(PWD + MAKEFILE)
+
+    for i in config.sections():
+        ShowDataProjectinSection(i)
+    
+    console.rule("[bold yellow]PROJECT FILES")
+    table = Table(show_lines= True,show_edge=True,box=box.SQUARE,expand=True)
+    table.add_column("Folder", justify="left", style="yellow bold", no_wrap=True)
+    table.add_column("Files", justify="left", style="green bold")
+    table.add_column("Size", justify="left", style="white bold",width=50)
+
+    if Section_general["template"] == "Basic":
+        FOLDERS = FOLDER_PROJECT_NEW 
+    else:
+        FOLDERS = FOLDER_PROJECT_8BP
+
+    for a in FOLDERS:
+        total_size = 0
+        files = glob.glob(PWD + a + "/*") 
+        contador = 0
+        for f in files: 
+            if int(f"{os.path.getsize(f)/float(1<<10):,.0f}") == 0:
+                total_size = total_size + 1
+                table.add_row(a,os.path.basename(f), "1 KB")
+            else:
+                total_size = total_size + int(f"{os.path.getsize(f)/float(1<<10):,.0f}")
+                table.add_row(a,os.path.basename(f), str(f"{os.path.getsize(f)/float(1<<10):,.0f} KB"))
+            contador += 1
+        if contador == 0:
+            table.add_row(a,"No files in folder", "0 KB")
+    console.print(table) 
+    print("")    
+
+
+def ShowDataProjectinSection(section):
+    Sectionitems  = readProyectSection(section)
+    console.rule("[bold yellow]"+section.upper())
+    table = Table(show_lines= True,show_edge=True,box=box.SQUARE,expand=True,show_header=False)
+    table.add_column("key", justify="left", style="green bold",max_width=7)
+    table.add_column("Value", justify="left", style="white bold")
+    for key,value in Sectionitems.items():
+            table.add_row(key, value)
+    console.print(table)  
+
+# Get number of kbytes of folder
+#   @Param folder project
+def GetTotalKbytesFolder(folder):
+    print("\n[yellow] files in " + folder +" ("+str(CountFilesFolderProject(folder))+"): ")
+    total_size = 0
+    tabla_files =[]
+    tabla_sizes =[]
+    files = glob.glob(PWD + folder + "/*") 
+    contador = 0
+    for f in files: 
+        if int(f"{os.path.getsize(f)/float(1<<10):,.0f}") == 0:
+            total_size = total_size + 1
+            print("[yellow]  -"+os.path.basename(f)+ " (1 KB)")
+        else:
+            total_size = total_size + int(f"{os.path.getsize(f)/float(1<<10):,.0f}")
+            print("[yellow]   -"+os.path.basename(f)+ str(f"{os.path.getsize(f)/float(1<<10):,.0f} KB"))
+        contador += 1
+    if contador == 0:
+        print("[yellow]  -No files in folder")
+        print("")
+
+    # return total_size
+
+# Count files in folder project
+#   @Param folder project
+def CountFilesFolderProject(folder):
+    try:
+        dir_path = r''+PWD+"/"+folder
+        count = 0
+        # Iterate directory
+        for path in os.listdir(dir_path):
+            # check if current path is a file
+            if os.path.isfile(os.path.join(dir_path, path)):
+                count += 1
+        return count
+    except IOError:
+        return 0
--- sdkcpc/validate.py
+++ sdkcpc/validate.py
@@ -6,53 +6,129 @@ from cerberus import Validator
 import os.path
 from os import path
 from .common import *
+from .info import *
 from rich import print
 from rich.console import Console
 console = Console(width=80,color_system="windows",force_terminal=True)
 
+COMPILATION = {}
+GENERAL = {}
+CONFIG = {}
+RVM = {}
+M4 = {}
+
+
+
 def conversions_error(error):
-    print()
-    show_info(MAKEFILE+" Validation Error","red")
+
+    show_info(MAKEFILE+" Validation","white")
     for line in error.splitlines():
-        if "build >" in line:
-            print("[red bold]\[ERROR][yellow bold] compilation -> build = [red bold]This key must exist and cannot be blank.")
-        elif "version >" in error:
-            print("[red bold]\[ERROR][yellow bold] compilation -> version = [red bold]This key does not contain version 0.0.0 format.")
-        elif "name >" in error:
-            print("[red bold]\[ERROR][yellow bold] general -> name = [red bold]This field must exist and can only have a maximum of 8 characters.") 
-        elif "description >" in error:
-            print("[red bold]\[ERROR][yellow bold] general -> description = [red bold]This key must exist.")
-        elif "template >" in error:
-            print("[red bold]\[ERROR][yellow bold] general -> template = [red bold]This key must exist. Supported values ​​are: Basic or 8BP.")
-        elif "authors >" in error:
-            print("[red bold]\[ERROR][yellow bold] general -> authors = [red bold]This key must exist and cannot be blank.")
-        elif "concatenate.bas.files >" in error:
-            print("[red bold]\[ERROR][yellow bold] config -> concatenate.bas.files = [red bold]This key must exist. Supported values ​​are: Yes or No.")
-        elif "config > name.bas.file >" in error:
-            print("[red bold]\[ERROR][yellow bold] config -> name.bas.file = [red bold]This field must exist and the value must comply with the 8:3 format, with extension '.bas'.")
-        elif "model.cpc >" in error:
-            print("[red bold]\[ERROR][yellow bold] rvm -> model.cpc = [red bold]This key must exist. Supported values ​​are: 464,664 or 6128.")
-        elif "ip >" in error:
-            print("[red bold]\[ERROR][yellow bold] m4 -> ip = [red bold]This field must exist and the value must comply with the IP format (0.0.0.0).")
+        
         # check sections
-        elif "compilation > required field" in error:
-            print("[red bold]\[ERROR][yellow bold] \[compilation] -> [red bold]This section not exist.")
-        elif "config > required field" in error:
+        if "compilation > required field" in line:
+            print("[red bold]\[*] [yellow bold]COMPILATION [red bold]This section not exist.")
+        elif "config > required field" in line:
             print("[red bold]\[ERROR][yellow bold] \[config] -> [red bold]This section not exist.")
-        elif "general > required field" in error:
+        elif "general > required field" in line:
             print("[red bold]\[ERROR][yellow bold] \[general] -> [red bold]This section not exist.")
-        elif "rvm > required field" in error:
+        elif "rvm > required field" in line:
             print("[red bold]\[ERROR][yellow bold] \[rvm] -> [red bold]This section not exist.")
-        elif "m4 > required field" in error:
+        elif "m4 > required field" in line:
             print("[red bold]\[ERROR][yellow bold] \[m4] -> [red bold]This section not exist.")
 
-    print()
+        if "build >" in line:
+            COMPILATION["build"] = "This key must exist and cannot be blank."
+        elif "version >" in line:
+            COMPILATION["version"]="This key does not contain version 0.0.0 format."
+        elif "name >" in line:
+            GENERAL["name"] = "This key must exist and can only have a maximum of 8 characters." 
+        elif "description >" in line:
+            GENERAL["description"] = "This key must exist."
+        elif "template >" in line:
+            GENERAL["template"] = "This key must exist. Supported values ​​are: Basic or 8BP."
+        elif "authors >" in line:
+            GENERAL["authors"] = "This key must exist and cannot be blank."
+        elif "concatenate.bas.files >" in line:
+            CONFIG["concatenate.bas.files"] = "This key must exist. Supported values ​​are: Yes or No."
+        elif "config > name.bas.file >" in line:
+            CONFIG["name.bas.file"] ="This key must exist and the value must comply with the 8:3 format, with extension '.bas'."
+        elif "model.cpc >" in line:
+            RVM["model.cpc"] = "This key must exist. Supported values ​​are: 464,664 or 6128."
+        elif "ip >" in line:
+            M4["ip"] = "This key must exist and the value must comply with the IP format (0.0.0.0)."
+    
+    show_specific_key_error ("COMPILATION",COMPILATION)
+    show_specific_key_error ("GENERAL",GENERAL)
+    show_specific_key_error ("CONFIG",CONFIG)
+    show_specific_key_error ("RVM",RVM)
+    show_specific_key_error ("M4",M4) 
+
+    
+def show_specific_key_error(section,diccionary):
+    if len(diccionary) > 0:
+        print("[*] [blue bold]"+ section +" [/]")
+        for key in diccionary:
+            print("[red bold]\[*]   [yellow bold]" + key + ": [red bold]" + diccionary[key])
+
+           
+
+    # print("[*] [blue bold]COMPILATION [/]------------------------------------------------------------")
+    # print("[*] - compilation: " + errors["compilation"]["build"])
+    # print("[*] - version: " + errors["compilation"]["version"])
+    # print("[*] [blue bold]GENERAL [/]----------------------------------------------------------------")
+    # print("[*] - name: " + errors["general"]["name"])
+    # print("[*] - description: " + errors["general"]["description"])
+    # print("[*] - template: " + errors["general"]["template"])
+    # print("[*] - authors: " + errors["general"]["authors"])
+    # print("[*] [blue bold]CONFIG [/]-----------------------------------------------------------------")
+    # print("[*] - concatenate.bas.files: " + errors["config"]["concatenate.bas.files"])
+    # print("[*] - name.bas.file: " + errors["config"]["name.bas.file"])
+    # print("[*] [blue bold]RETRO VIRTUAL MACHINE [/]--------------------------------------------------")
+    # print("[*] - model.cpc: " + errors["rvm"]["model.cpc"])
+    # print("[*] [blue bold]M4 BOARD [/]---------------------------------------------------------------")
+    # print("[*] - ip: " + errors["m4"]["ip"])
+    # print(" ")            
+    # for line in error.splitlines():
+    #     if "build >" in line:
+    #         print("[red bold]\[ERROR][yellow bold] compilation -> build = [red bold]This key must exist and cannot be blank.")
+    #     elif "version >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] compilation -> version = [red bold]This key does not contain version 0.0.0 format.")
+    #     elif "name >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] general -> name = [red bold]This field must exist and can only have a maximum of 8 characters.") 
+    #     elif "description >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] general -> description = [red bold]This key must exist.")
+    #     elif "template >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] general -> template = [red bold]This key must exist. Supported values ​​are: Basic or 8BP.")
+    #     elif "authors >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] general -> authors = [red bold]This key must exist and cannot be blank.")
+    #     elif "concatenate.bas.files >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] config -> concatenate.bas.files = [red bold]This key must exist. Supported values ​​are: Yes or No.")
+    #     elif "config > name.bas.file >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] config -> name.bas.file = [red bold]This field must exist and the value must comply with the 8:3 format, with extension '.bas'.")
+    #     elif "model.cpc >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] rvm -> model.cpc = [red bold]This key must exist. Supported values ​​are: 464,664 or 6128.")
+    #     elif "ip >" in error:
+    #         print("[red bold]\[ERROR][yellow bold] m4 -> ip = [red bold]This field must exist and the value must comply with the IP format (0.0.0.0).")
+    #     # check sections
+    #     elif "compilation > required field" in error:
+    #         print("[red bold]\[ERROR][yellow bold] \[compilation] -> [red bold]This section not exist.")
+    #     elif "config > required field" in error:
+    #         print("[red bold]\[ERROR][yellow bold] \[config] -> [red bold]This section not exist.")
+    #     elif "general > required field" in error:
+    #         print("[red bold]\[ERROR][yellow bold] \[general] -> [red bold]This section not exist.")
+    #     elif "rvm > required field" in error:
+    #         print("[red bold]\[ERROR][yellow bold] \[rvm] -> [red bold]This section not exist.")
+    #     elif "m4 > required field" in error:
+    #         print("[red bold]\[ERROR][yellow bold] \[m4] -> [red bold]This section not exist.")
+
+
             
 def show_error_validate(error):
     new_error = re.sub('[^a-zA-Z0-9:, \n\.]', '', error)
     new_error = new_error.replace(":", " >")
     new_error = new_error.replace(",", "\n")
     conversions_error(new_error)
+
     # print ("[red bold]\nValidation " + MAKEFILE + " Error:\n " + new_error)
 
 
@@ -65,8 +141,32 @@ def validate_data_project():
 
     if not v.validate(doc, schema):
         show_error_validate(str(v.errors))
+        show_foot("Validation ERROR","red")
+        print()
         sys.exit(1)
 
+
+    show_head("PROJECT.CFG VALIDATION","white")
+    # print("[*] ------------------------------------------------------------------------")
+    print("[green bold][OK][/] [blue bold]COMPILATION [/]")
+    print("[green bold][OK][/]   compilation: " + project_data["compilation"]["build"])
+    print("[green bold][OK][/]   version: " + project_data["compilation"]["version"])
+    print("[green bold][OK][/] [blue bold]GENERAL [/]")
+    print("[green bold][OK][/]   name: " + project_data["general"]["name"])
+    print("[green bold][OK][/]   description: " + project_data["general"]["description"])
+    print("[green bold][OK][/]   template: " + project_data["general"]["template"])
+    print("[green bold][OK][/]   authors: " + project_data["general"]["authors"])
+    print("[green bold][OK][/] [blue bold]CONFIG [/]")
+    print("[green bold][OK][/]   concatenate.bas.files: " + project_data["config"]["concatenate.bas.files"])
+    print("[green bold][OK][/]   name.bas.file: " + project_data["config"]["name.bas.file"])
+    print("[green bold][OK][/] [blue bold]RETRO VIRTUAL MACHINE [/]")
+    print("[green bold][OK][/]   model.cpc: " + project_data["rvm"]["model.cpc"])
+    print("[green bold][OK][/] [blue bold]M4 BOARD [/]")
+    print("[green bold][OK][/]   ip: " + project_data["m4"]["ip"])
+    show_foot("Validation Successfull","green")
+    
+    
+    
 # validate folder is poject
 def validate_Folder_Project():
     if not os.path.exists(PWD + MAKEFILE):
